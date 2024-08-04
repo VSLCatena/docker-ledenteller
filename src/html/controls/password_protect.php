40d83884b7bed81041290416455d3450c6e89090
Martijn <12066560+Kipjr@users.noreply.github.com>
2024-08-04 14:04:57 +0200

    Update password_protect.php

--- src/html/controls/password_protect.php
+++ src/html/controls/password_protect.php
@@ -70,7 +70,7 @@ $LOGIN_INFORMATION = array(
 define('USE_USERNAME', (bool)getenv_docker('USE_USERNAME', 0));
 
 // User will be redirected to this page after logout
-define('LOGOUT_URL', getenv_docker('LOGOUT_URL', 'about://blank'));
+define('LOGOUT_URL', getenv_docker('LOGOUT_URL', '../index.php'));
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
 define('TIMEOUT_MINUTES', (int)getenv_docker('TIMEOUT_MINUTES', 120));
